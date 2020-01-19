ce5babac61e6dc297803fdca71235ba9956f85a6
Eric Yang <43188567+haoyuanyang929@users.noreply.github.com>
2020-01-19 07:45:57 -0800

    Make the Scatter plot axis ordered

--- ConversionCode/graphtheBluePrint.py
+++ ConversionCode/graphtheBluePrint.py
@@ -9,10 +9,10 @@ with open('dataFile.csv') as file:
     col = 0
     csv_reader = csv.reader(file, delimiter=',')
     for row in csv_reader:
-        id.append(row[0])
-        xID.append(row[1])
-        yID.append(row[2])
+        id.append(int(row[0]))
+        xID.append(float(row[1]))
+        yID.append(float(row[2]))
 
-    plot.scatter(xID,yID,s=np.pi*3,c=(0,0,0),alpha=0.5)
+    plot.scatter(xID,yID,s=np.pi*3,alpha=0.5)
     plot.title('Blueprint')
     plot.show()
