4e2ebb1e8b19ca08d16889de01bb35d4e3cb4811
gdbt <45052215+gdbt@users.noreply.github.com>
2020-01-19 04:35:44 -0800

    Add files via upload
    Began setting up mysql access.

--- Android App/app/src/main/java/com/example/noah/robotcontroller/mysql.java
+++ Android App/app/src/main/java/com/example/noah/robotcontroller/mysql.java
@@ -1,13 +1,40 @@
 package com.example.noah.robotcontroller;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class mysql extends AppCompatActivity {
-
+String url = "";
+String uid = "phone";
+String pwd = "almostdoneguys";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mysql);
+
+    }
+    private class mysqlconnect extends AsyncTask<String,Void,String>{
+        String res ="";
+        protected void onPreExecute(){
+            super.onPreExecute();
+            Toast.makeText(mysql.this,"Please wait...", Toast.LENGTH_SHORT).show();
+        }
+        protected String doInBackground(String...params){
+            try{
+                Class.forName("com.mysql.cj.jdbc.Drive");
+                Connection con = DriverManager.getConnection(url,uid,pwd);
+                String sql = "INSERT INTO locationMatrix (xnum, ynum) VALUES(?,?)";
+                PreparedStatement stmt = con.prepareStatement(sql);
+                stmt.executeUpdate();
+            }catch (Exception e){
+
+            }
+        }
     }
 }
