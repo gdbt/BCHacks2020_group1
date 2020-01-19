88c7a51399afd8a3fa8a4a9611e6b485f0f1f43d
gdbt <45052215+gdbt@users.noreply.github.com>
2020-01-19 06:43:48 -0800

    Add files via upload
    Created a basic blue tooth design. Creates an array and then sends the array over blue tooth.

---
+++ bluetooth/basicbluetooth.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(0,1);
+char data = 0;
+void setup() {
+  BTserial.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int x[10] = {1,2,3,4,5,6,7,8,9,10};
+  if(Serial.available() > 0){
+    data = Serial.read();
+    if(data == '2'){
+      int i;
+      for(i = 0; i < 9; i++){
+          Serial.print(x[i]);
+          Serial.print(",");
+          Serial.println(x[i+1]);
+      }
+      
+    }
+  }
+
+}
